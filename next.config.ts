a8e7a5b045f31e2e9869e4627e851cf42ce27974
Lutfi Ihsan <36567675+lutfiihsan@users.noreply.github.com>
2026-05-09 10:27:32 +0700

    fix: update image remote patterns to allow all supabase domains

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'wzyxmuycxlycgjqplnwh.supabase.co',
+        hostname: '*.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
